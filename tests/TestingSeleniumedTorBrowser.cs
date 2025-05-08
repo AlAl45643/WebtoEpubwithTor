c76da34c80951b8df1b1d982f640b264a4f97b2f
AlAl <AlAlwork@protonmail.com>
2025-05-07 20:17:38 -0400

    Added Are_links_bodys_retrieved_correctly() test case for retrieving html from links; Added Are_links_bodys_converted_to_EPUB_correctly() test case to check if a valid epub is generated from content in chapters

--- tests/TestingSeleniumedTorBrowser.cs
+++ tests/TestingSeleniumedTorBrowser.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System.IO.Compression;
+using System.IO;
+using System.Diagnostics;
 
 namespace tests
 {
@@ -16,9 +18,10 @@ namespace tests
             FirefoxDriverService firefoxDriverService;
             FirefoxDriver firefoxDriver;
             FirefoxOptions firefoxOptions;
-            string profilePath = "/home/work/tor-browser-linux-x86_64-14.0.7/tor-browser/Browser/TorBrowser/Data/Browser/profile.default/";
-            string binaryPath = "/home/work/tor-browser-linux-x86_64-14.0.7/tor-browser/Browser/firefox";
-            string geckoDriverPath = "/home/work/tor-browser-linux-x86_64-14.0.7/tor-browser/geckodriver-v0.36.0-linux64/geckodriver";
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string profilePath = currentDirectory + "/resources/tor-browser/Browser/TorBrowser/Data/Browser/profile.default/";
+            string binaryPath = currentDirectory + "/resources/tor-browser/Browser/firefox";
+            string geckoDriverPath = currentDirectory + "/resources/tor-browser/geckodriver";
 
             firefoxDriverService = FirefoxDriverService.CreateDefaultService(geckoDriverPath);
             firefoxDriverService.FirefoxBinaryPath = binaryPath;
@@ -30,6 +33,7 @@ namespace tests
             };
             firefoxOptions.AddArguments("-profile", profilePath);
             firefoxOptions.SetPreference("marionette.debugging.clicktostart", false);
+            firefoxOptions.SetPreference("torbrowser.settings.quickstart.enabled", true);
 
             firefoxDriver = new FirefoxDriver(firefoxDriverService, firefoxOptions, TimeSpan.FromSeconds(180));
             return firefoxDriver;
@@ -124,8 +128,8 @@ namespace tests
             List<string> returnedLinks = [];
             List<string> mockChapters =
             [
-                 "This is chapter 1",
-                 "This is chapter 2"
+             "This is chapter 1",
+             "This is chapter 2"
             ];
             List<string> returnedChapters = [];
 
@@ -168,71 +172,115 @@ namespace tests
         /// </summary>
         public void Are_links_bodys_converted_to_EPUB_correctly()
         {
-            string currentDirectory = Directory.GetCurrentDirectory();
-            string epubDirectory = Path.Combine(currentDirectory, "resources", "epub");
-            string epubZipDirectory = Path.Combine(currentDirectory, "resources", "epub.zip");
-            string epubContentDirectory = Path.Combine(currentDirectory, "resources", "epub", "OEBPS");
+            string currentExecutionPath = Directory.GetCurrentDirectory();
+            string pathToEpubRoot = Path.Combine(currentExecutionPath, "resources", "epub");
+            string pathToZipEpubAt = Path.Combine(currentExecutionPath, "resources", "epub.epub");
+            string pathToEpubContent = Path.Combine(currentExecutionPath, "resources", "epub", "OEBPS");
+            string pathToMimetypeFile = Path.Combine(currentExecutionPath, "resources", "mimetype");
+            string pathToEpubValidator = Path.Combine(currentExecutionPath, "resources", "epubcheck-5.2.1", "epubcheck.jar");
+            Process epubValidatorProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "java",
+                    Arguments = $"-jar {pathToEpubValidator} {pathToZipEpubAt}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
             List<string> stubChapters =
             [
                 "This is chapter 1",
                 "This is chapter 2"
             ];
-            string uid = $"wet{DateTime.Now}";
+            string epubUID = $"wet/{DateTime.Now}";
+            string epubValidatorResult = "";
 
-            // create xhtml chapters in epubContentDirectory
+            // create xhtml chapters in pathToEpubContent
             for (int i = 0; i < stubChapters.Count; i++)
             {
-                using (StreamWriter chapterFile = new(Path.Combine(epubContentDirectory, $"{i}.xhtml")))
+                using (StreamWriter chapterFile = new(Path.Combine(pathToEpubContent, $"{i + 1}.xhtml")))
                 {
-                    chapterFile.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head>\n    <title>{i}</title>\n</head>\n<body>\n");
+                    chapterFile.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head>\n    <title>{i + 1}</title>\n</head>\n<body>\n<p>");
                     chapterFile.Write(stubChapters[i]);
-                    chapterFile.Write($"\n</body>\n</html>");
+                    chapterFile.Write($"</p>\n</body>\n</html>");
 
                 }
             }
-
-            var files = Directory.EnumerateFiles(epubContentDirectory);
-
-            foreach (var file in files)
+            // create content.opf in pathToEpubContent
+            using (StreamWriter contentopf = new(Path.Combine(pathToEpubContent, "content.opf")))
             {
-                Console.WriteLine(file);
-            }
-
-            // create content.opf in epubContentDirectory
-            using (StreamWriter contentopf = new(Path.Combine(epubContentDirectory, "content.opf")))
-            {
-                contentopf.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"??>\n<package xmlns=\"http://www.idpf.org/2007/opf\" unique-identifier=\"BookID\" version=\"2.0\">\n   <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\">\n        <dc:title>WET</dc:title>\n        <dc:creator opf:role=\"aut\">Yoda47</dc:creator>\n        <dc:language>en-US</dc:language>\n        <dc:rights>Public Domain</dc:rights>\n        <dc:publisher>WET</dc:publisher>\n        <dc:identifier id=\"BookID\" opf:scheme=\"UUID\">{uid}</dc:identifier>\n    </metadata>\n    <manifest>\n");
+                contentopf.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" unique-identifier=\"BookID\" version=\"2.0\">\n   <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:opf=\"http://www.idpf.org/2007/opf\">\n        <dc:title>WET</dc:title>\n        <dc:creator opf:role=\"aut\">WET</dc:creator>\n        <dc:language>en-US</dc:language>\n        <dc:rights>Public Domain</dc:rights>\n        <dc:publisher>WET</dc:publisher>\n        <dc:identifier id=\"BookID\" opf:scheme=\"UUID\">{epubUID}</dc:identifier>\n    </metadata>\n    <manifest>\n        <item id=\"ncx\" href=\"toc.ncx\" media-type=\"application/x-dtbncx+xml\" />\n");
 
                 for (int i = 0; i < stubChapters.Count; i++)
                 {
-                    contentopf.Write($"<item id=\"page{i}\" href=\"{i}.xhtml\" media-type=\"application / xhtml + xml\" />");
+                    contentopf.Write($"      <item id=\"page{i + 1}\" href=\"{i + 1}.xhtml\" media-type=\"application/xhtml+xml\" />\n");
                 }
 
                 contentopf.Write($"    </manifest>\n    <spine toc=\"ncx\">\n");
 
                 for (int i = 0; i < stubChapters.Count; i++)
                 {
-                    contentopf.Write($"<itemref idref=\"page{i}\" />");
+                    contentopf.Write($"       <itemref idref=\"page{i + 1}\" />\n");
                 }
 
                 contentopf.Write($"    </spine>\n</package>");
             }
-
-            // create toc.ncx
-            using (StreamWriter tocncx = new(Path.Combine(epubContentDirectory, "toc.ncx")))
+            // create toc.ncx in pathToEpubContent
+            using (StreamWriter tocncx = new(Path.Combine(pathToEpubContent, "toc.ncx")))
             {
-                tocncx.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ncx xmlns=\"http://www.daisy.org/z3986/2005/ncx/\" version=\"2005-1\">\n\n<head>\n    <meta name=\"dtb:uid\" content=\"{uid}\"/>    <meta name=\"dtb:depth\" content=\"1\"/>\n    <meta name=\"dtb:totalPageCount\" content=\"0\"/>\n    <meta name=\"dtb:maxPageNumber\" content=\"0\"/>\n</head>\n\n<docTitle>\n    <text>WET</text>\n</docTitle>\n\n<navMap>\n");
+                tocncx.Write($"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ncx xmlns=\"http://www.daisy.org/z3986/2005/ncx/\" version=\"2005-1\">\n\n<head>\n    <meta name=\"dtb:epubUID\" content=\"{epubUID}\"/>    <meta name=\"dtb:depth\" content=\"1\"/>\n    <meta name=\"dtb:totalPageCount\" content=\"0\"/>\n    <meta name=\"dtb:maxPageNumber\" content=\"0\"/>\n</head>\n\n<docTitle>\n    <text>WET</text>\n</docTitle>\n\n<navMap>\n");
 
                 for (int i = 0; i < stubChapters.Count; i++)
                 {
-                    tocncx.Write($"    <navPoint id=\"chapter{i}\" playOrder=\"{i}\">\n        <navLabel>\n        <text>Chapter {i}</text>\n    </navLabel>\n    <content src=\"{i}.xhtml\"/>\n</navPoint>");
+                    tocncx.Write($"    <navPoint id=\"page{i + 1}\" playOrder=\"{i + 1}\">\n        <navLabel>\n        <text>Chapter {i + 1}</text>\n    </navLabel>\n    <content src=\"{i + 1}.xhtml\"/>\n</navPoint>\n");
                 }
 
                 tocncx.Write("\n\n</navMap>\n</ncx>");
             }
+            // an epub file is just a zip with a .epub extension
+            using (FileStream zipFile = new FileStream(pathToZipEpubAt, FileMode.Create))
+            {
+                using (ZipArchive zipArchive = new ZipArchive(zipFile, ZipArchiveMode.Create))
+                {
+                    // mimetype file must come first and be uncompressed according to epub specifications
+                    zipArchive.CreateEntryFromFile(pathToMimetypeFile, Path.Combine("mimetype"), CompressionLevel.NoCompression);
+                    RecursiveEntry(zipArchive, pathToEpubRoot, "");
+                }
+            }
 
-            // zip up epub
-            ZipFile.CreateFromDirectory(epubDirectory, epubZipDirectory);
+            epubValidatorProcess.Start();
+            while (!epubValidatorProcess.StandardOutput.EndOfStream)
+            {
+                epubValidatorResult += epubValidatorProcess.StandardOutput.ReadLine();
+            }
+            Assert.That(epubValidatorResult.Contains("0 errors") && epubValidatorResult.Contains("0 fatals"));
+
         }
+
+        /// <summary>
+        /// Add entries in 'directory' to 'archive' as 'pathInZip' recursively if entry is a directory.
+        /// </summary>
+        private void RecursiveEntry(ZipArchive zipArchive, string directory, string pathInZip)
+        {
+            var entries = Directory.EnumerateFileSystemEntries(directory);
+            foreach (var entry in entries)
+            {
+                string entryFileName = Regex.Match(entry, "(?!.*[\\/]).*").Value;
+                if (File.GetAttributes(entry).HasFlag(FileAttributes.Directory))
+                {
+                    string newPathInZip = Path.Combine(pathInZip, entryFileName);
+                    RecursiveEntry(zipArchive, entry, newPathInZip);
+                    continue;
+                }
+                zipArchive.CreateEntryFromFile(entry, Path.Combine(pathInZip, entryFileName));
+            }
+            return;
+        }
+
+
+
     }
+
 }
